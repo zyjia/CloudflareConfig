3c6338701ee8a50107722afcd97dd9c36475b903
TonyZhang <tonyzhang1995@163.com>
2024-01-30 12:56:48 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,24 @@
 <?php
 
+function requestAPI($url, $data) {
+
+  // 初始化curl
+  $ch = curl_init();
+
+  // 设置请求选项 
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+  // 执行请求
+  $result = curl_exec($ch);
+
+  // 关闭curl
+  curl_close($ch);
+
+  return $result;
+
+}
 
 $ipFile = 'ip/ipv4.txt';
 $ipAddresses = file($ipFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
